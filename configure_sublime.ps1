5c51d46ecde3b0834fc1a3f82a8aa0e218c67f3f
Fabian Wetzel <fabsenet@googlemail.com>
2014-06-13 22:49:31 +0200

    added installation of sublimetext_indentxml package to sublime

--- configure_sublime.ps1
+++ configure_sublime.ps1
@@ -1,1 +1,19 @@
 ﻿robocopy '.\Sublime3' "$env:APPDATA\Sublime Text 3" /W:1 /R:3
+
+
+
+#install sublimetext_indentxml
+Push-Location
+cd C:\github
+if(![System.IO.Directory]::Exists("C:\github\alek-sys\sublimetext_indentxml"))
+{
+	git clone https://github.com/alek-sys/sublimetext_indentxml.git alek-sys\sublimetext_indentxml
+	cd alek-sys\sublimetext_indentxml
+}
+else
+{
+	cd alek-sys\sublimetext_indentxml
+	git pull
+}
+robocopy '.\' "$env:APPDATA\Sublime Text 3\Packages\sublimetext_indentxml" /W:1 /R:3
+Pop-Location
