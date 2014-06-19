c623b7c82e208cf89ff0d7ca3508257c6aefd25e
Fabian Wetzel <fabsenet@googlemail.com>
2014-06-19 16:41:57 +0200

    added install step for hasher plugin

--- configure_sublime.ps1
+++ configure_sublime.ps1
@@ -16,4 +16,20 @@ else
 	git pull
 }
 robocopy '.\' "$env:APPDATA\Sublime Text 3\Packages\sublimetext_indentxml" /W:1 /R:3
+
+
+#install hasher
+Push-Location
+cd C:\github
+if(![System.IO.Directory]::Exists("C:\github\dangelov\hasher"))
+{
+	git clone https://github.com/dangelov/hasher.git dangelov\hasher
+	cd dangelov\hasher
+}
+else
+{
+	cd dangelov\hasher
+	git pull
+}
+robocopy '.\' "$env:APPDATA\Sublime Text 3\Packages\hasher" /W:1 /R:3
 Pop-Location
